c0d9b2056f36464431f2af33b2d56bc5846c6715
redkinikita <110107967+redkinikita@users.noreply.github.com>
2022-11-09 16:11:39 +0100

    Project summary

--- Responce_Function.py
+++ Responce_Function.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.constants as sp
 import scipy.fft as fft
-from tqdm import tqdm
+
 
 
 def refractive_index(w, w_TO, w_LO, gamm, epsilInf):
